3256157a12ec5f62ffb4976b6c0780ef72db9c26
hongqiang.ren <820941512@qq.com>
2018-05-17 18:00:15 +0800

    add algorithm support

--- src/main/java/ren/com/cn/controller/WechatTokenController.java
+++ src/main/java/ren/com/cn/controller/WechatTokenController.java
@@ -1,15 +1,15 @@
 package ren.com.cn.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Comparator;
-import java.util.TreeMap;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
  * @author : hongqiangren.
@@ -20,20 +20,44 @@ import java.util.TreeMap;
 @Slf4j
 public class WechatTokenController {
 
+    private final static String  TOKEN = "ren111";
+
     @RequestMapping(value = {"/token"}, method = RequestMethod.GET)
     @ResponseBody
-    public String getToken(@RequestParam(name = "signature") String signature,
-                           @RequestParam(name = "timestamp") Long timestamp,
-                           @RequestParam(name = "nonce") String nonce,
-                           @RequestParam(name = "echostr")String echostr) {
-        TreeMap<String,String> map = new TreeMap(Comparator.naturalOrder());
+    public String getToken(@RequestParam(name = "signature",required = false) String signature,
+                           @RequestParam(name = "timestamp",required = false) String timestamp,
+                           @RequestParam(name = "nonce",required = false) String nonce,
+                           @RequestParam(name = "echostr",required = false)String echostr) throws NoSuchAlgorithmException {
         log.info("nonce:{},signature:{},timestamp:{},echostr:{}",nonce,signature,timestamp,echostr);
-        map.put("nonce",nonce);
-        map.put("signature",signature);
-        map.put("timestamp",String.valueOf(timestamp));
-        String compute = DigestUtils.sha1Hex(nonce + signature + timestamp);
-        log.info("wechat echostr:----{}-----,campute result:{}",echostr,compute);
+        String signHex = getSignHex(timestamp,nonce);
+        log.info("wechat signature:----{}-----,campute signature:{}",signature,signHex);
         return echostr;
+    }
+
+    private String getSignHex(String timestamp, String nonce) throws NoSuchAlgorithmException {
+        String[] array = new String[] { TOKEN, timestamp, nonce};
+        StringBuffer sb = new StringBuffer();
+        // 字符串排序
+        Arrays.sort(array);
+        for (int i = 0; i < 4; i++) {
+            sb.append(array[i]);
+        }
+        String str = sb.toString();
+        // SHA1签名生成
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+
+        StringBuffer hexstr = new StringBuffer();
+        String shaHex = "";
+        for (int i = 0; i < digest.length; i++) {
+            shaHex = Integer.toHexString(digest[i] & 0xFF);
+            if (shaHex.length() < 2) {
+                hexstr.append(0);
+            }
+            hexstr.append(shaHex);
+        }
+        return hexstr.toString();
     }
 
 
